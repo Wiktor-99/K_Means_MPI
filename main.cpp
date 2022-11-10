a7584b54ba31eaac7bebdae49227de75adce191c
Wiktor Bajor <wiktorbajor1@gmail.com>
2022-11-10 17:09:35 +0100

    Change Data representation

--- main.cpp
+++ main.cpp
@@ -5,47 +5,42 @@
 #include <random>
 #include <algorithm>
 
-struct Image
-{
-    void appendPixel(int r, int g, int b){
-        redPixel.push_back(r);
-        greenPixel.push_back(g);
-        bluePixel.push_back(b);
-    }
-
-    std::vector<int> redPixel;
-    std::vector<int> greenPixel;
-    std::vector<int> bluePixel;
+struct Pixel {
+    int r;
+    int g;
+    int b;
 };
 
-Image getImageFromFile(std::string fileName, int width, int hight){
+std::vector<Pixel> getImageFromFile(std::string fileName, int width, int hight){
     std::fstream file{fileName};
-    Image output;
+    std::vector<Pixel>  output;
     for (int i = 0; i < width * hight; ++i){
         int r, g, b;
         file >> r >> g >> b;
-        output.appendPixel(r, g, b);
+        output.push_back({r, g, b});
     }
     return output;
 }
 
-void imageToFile(std::string fileName, Image& image){
+void imageToFile(std::string fileName, const std::vector<Pixel>& image){
     std::fstream file{fileName};
-    for (int i = 0; i < image.redPixel.size(); ++i){
-        file << image.redPixel[i] << ' ' << image.greenPixel[i] << ' ' << image.bluePixel[i] << ' ';
+    for (int i = 0; i < image.size(); ++i){
+        file << image[i].r << ' ' << image[i].g << ' ' << image[i].b << ' ';
     }
 }
 
-double euclideanDistance(int red, int green, int blue, int x, int y, int z){
-    return std::sqrt(std::pow(red - x, 2) + std::pow(green - y, 2) + std::pow(blue - z, 2));
+double euclideanDistance(Pixel imagePixel, Pixel clusterPixel){
+    return std::sqrt(std::pow(imagePixel.r - clusterPixel.r, 2) +
+                     std::pow(imagePixel.g - clusterPixel.g, 2) +
+                     std::pow(imagePixel.b - clusterPixel.b, 2));
 }
 
-int assignCluster(int r,int g, int b, const Image& image , int clusters)
+int assignCluster(Pixel imagePixel, const std::vector<Pixel>& clusters)
 {
     std::vector<int> distances;
-	for (int i = 0; i < clusters; ++i)
+	for (const auto& clusterPixel : clusters)
 	{
-		int distance = euclideanDistance(r, g, b, image.redPixel[i], image.greenPixel[i], image.bluePixel[i]);
+		int distance = euclideanDistance(imagePixel, clusterPixel);
         distances.push_back(distance);
 
 	}
@@ -57,60 +52,48 @@ int main(){
     int width{512};
     int hight{512};
     auto image = getImageFromFile(fileName, width, hight);
-    int centroids{7};
+    int centroids{5};
 
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distr(0, 512*512-1);
 
-    Image clusterPoints2;
+    std::vector<Pixel> clusterPoints;
 
 
     for (int i = 0; i < centroids; i++){
         auto pixelIndex = distr(gen);
-        clusterPoints2.appendPixel(image.redPixel[pixelIndex], image.greenPixel[pixelIndex], image.bluePixel[pixelIndex]);
+        clusterPoints.push_back(image[pixelIndex]);
     }
 
-    std::vector<int> assignment(image.redPixel.size());
-    for (int iterations = 0; iterations < 100; ++iterations){
+    std::vector<int> assignment(image.size());
+    for (int iterations = 0; iterations < 500; ++iterations){
         std::vector<std::vector<int>> clusters(centroids);
 
-        for (int i = 0; i < image.redPixel.size(); ++i) {
-            auto clusterIndex = assignCluster(image.redPixel[i], image.greenPixel[i], image.bluePixel[i], clusterPoints2, centroids);
+        for (int i = 0; i < image.size(); ++i) {
+            auto clusterIndex = assignCluster(image[i], clusterPoints);
             clusters[clusterIndex].push_back(i);
             assignment[i] = clusterIndex;
         }
 
-
-        Image clusterPoints;
-        clusterPoints.redPixel = std::vector<int>(centroids);
-        clusterPoints.greenPixel = std::vector<int>(centroids);
-        clusterPoints.bluePixel = std::vector<int>(centroids);
-
         for (int i = 0; i < centroids; ++i){
             for (int j = 0;j < clusters[i].size(); ++j) {
-            clusterPoints.redPixel[i] += image.redPixel[clusters[i][j]];
-            clusterPoints.greenPixel[i] += image.greenPixel[clusters[i][j]];
-            clusterPoints.bluePixel[i] += image.bluePixel[clusters[i][j]];
+                clusterPoints[i].r += image[clusters[i][j]].r;
+                clusterPoints[i].g += image[clusters[i][j]].g;
+                clusterPoints[i].b += image[clusters[i][j]].b;
             }
-            clusterPoints.redPixel[i] /= clusters[i].size();
-            clusterPoints.greenPixel[i] /= clusters[i].size();
-            clusterPoints.bluePixel[i] /= clusters[i].size();
+            clusterPoints[i].r /= clusters[i].size();
+            clusterPoints[i].g /= clusters[i].size();
+            clusterPoints[i].b /= clusters[i].size();
         }
-
-        clusterPoints2 = clusterPoints;
     }
 
-    Image output;
+    std::vector<Pixel>  output;
 
     for (int i = 0; i < width * hight; ++i) {
         int clusterIndex = assignment[i];
-        int red = clusterPoints2.redPixel[clusterIndex];
-        int green = clusterPoints2.greenPixel[clusterIndex];
-        int blue = clusterPoints2.bluePixel[clusterIndex];
-
-        output.appendPixel(red, green, blue);
+        output.push_back(clusterPoints[clusterIndex]);
     }
 
 
